371c0f278c1fc7cb8f197423a53929ec24db9ecf
VagariesOfFate <64036186+VagariesOfFate@users.noreply.github.com>
2026-09-22 10:04:39 -0600

    Repair scorer exports formatting

--- src/cdm/__init__.py
+++ src/cdm/__init__.py
@@ -1,13 +1,20 @@
 """Code Decision Model experimental package."""
 
-from .model import (\n    CodeDecisionModel,\n    CosineMixScorer,\n    EncodedDecisionContext,\n    PairwiseMLPScorer,\n)
+from .model import (
+    CodeDecisionModel,
+    CosineMixScorer,
+    EncodedDecisionContext,
+    PairwiseMLPScorer,
+)
 from .text import HashTextEncoder
 from .unixcoder import UniXcoderEncoder
 from .coderank import CodeRankEncoder
 
 __all__ = [
-    "CodeDecisionModel",\n    "CosineMixScorer",
-    "EncodedDecisionContext",\n    "PairwiseMLPScorer",
+    "CodeDecisionModel",
+    "CosineMixScorer",
+    "EncodedDecisionContext",
+    "PairwiseMLPScorer",
     "HashTextEncoder",
     "UniXcoderEncoder",
     "CodeRankEncoder",
