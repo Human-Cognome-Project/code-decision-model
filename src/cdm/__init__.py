628aaf312f65207756948e7ba50d8f378417a2d1
VagariesOfFate <64036186+VagariesOfFate@users.noreply.github.com>
2026-09-22 07:46:24 -0600

    Add dynamic decision MVP scaffold

---
+++ src/cdm/__init__.py
@@ -0,0 +1,6 @@
+"""Code Decision Model experimental package."""
+
+from .model import CodeDecisionModel, EncodedDecisionContext
+from .text import HashTextEncoder
+
+__all__ = ["CodeDecisionModel", "EncodedDecisionContext", "HashTextEncoder"]
