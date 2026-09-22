d7a339cea3ac5a55d44ffad2b1bc508fa748d2ce
VagariesOfFate <64036186+VagariesOfFate@users.noreply.github.com>
2026-09-22 11:13:26 -0600

    Export corrective-turn harness

--- src/cdm/__init__.py
+++ src/cdm/__init__.py
@@ -10,6 +10,20 @@ from .text import HashTextEncoder
 from .unixcoder import UniXcoderEncoder
 from .coderank import CodeRankEncoder
 from .potion import PotionCodeEncoder
+from .corrections import (
+    CandidateGenerator,
+    CandidateSelector,
+    CandidateValidator,
+    CorrectionAttempt,
+    CorrectionEpisode,
+    CorrectionTask,
+    FirstCandidateSelector,
+    PairedCorrectionResult,
+    ScoreSelector,
+    ValidationResult,
+    run_correction_episode,
+    run_paired_correction,
+)
 from .constraints import (
     AllAllowed,
     ConstrainedDecision,
@@ -29,6 +43,18 @@ __all__ = [
     "UniXcoderEncoder",
     "CodeRankEncoder",
     "PotionCodeEncoder",
+    "CandidateGenerator",
+    "CandidateSelector",
+    "CandidateValidator",
+    "CorrectionAttempt",
+    "CorrectionEpisode",
+    "CorrectionTask",
+    "FirstCandidateSelector",
+    "PairedCorrectionResult",
+    "ScoreSelector",
+    "ValidationResult",
+    "run_correction_episode",
+    "run_paired_correction",
     "AllAllowed",
     "ConstrainedDecision",
     "Constraint",
