c142e2aa59ca12253b623997ec26814635303c6b
VagariesOfFate <64036186+VagariesOfFate@users.noreply.github.com>
2026-09-22 09:42:12 -0600

    Update src/cdm/__init__.py for CodeRank

--- src/cdm/__init__.py
+++ src/cdm/__init__.py
@@ -3,10 +3,12 @@
 from .model import CodeDecisionModel, EncodedDecisionContext
 from .text import HashTextEncoder
 from .unixcoder import UniXcoderEncoder
+from .coderank import CodeRankEncoder
 
 __all__ = [
     "CodeDecisionModel",
     "EncodedDecisionContext",
     "HashTextEncoder",
     "UniXcoderEncoder",
+    "CodeRankEncoder",
 ]
